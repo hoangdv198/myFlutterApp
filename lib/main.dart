2f27cf45ad88bb8b9f6944e1025e9d6659134be2
hoangdv198 <hoangdv198@gmail.com>
2023-05-31 13:50:17 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, world! 1zzzz',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
