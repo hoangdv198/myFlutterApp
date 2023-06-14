7e9f9793c2dfa971d7bb6c263dd1d28ad89314da
hoangdv198 <hoangdv198@gmail.com>
2023-06-14 15:29:18 +0700

    uodate tutorials

--- lib/features/counter/cubit/counter_cubit.dart
+++ lib/features/counter/cubit/counter_cubit.dart
@@ -10,8 +10,8 @@ class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
   /// Add 1 to the current state.
-  void increment() => emit(state + 1);
+  void increment() => emit(state + 12);
 
   /// Subtract 1 from the current state.
-  void decrement() => emit(state - 1);
+  void decrement() => emit(state - 13);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
-import 'package:my_flutter_app/app.dart';
+import 'package:my_flutter_app/features/counter/counter_app.dart';
 import 'package:my_flutter_app/features/counter/couter_observer.dart';
 
 void main() {
