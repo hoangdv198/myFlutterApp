8f2f092f2e5fdeba6ddf98ad2ca053830e3c06a7
hoangdv198 <hoangdv198@gmail.com>
2023-06-30 17:13:18 +0700

    create base

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:my_flutter_app/features/counter/counter_app.dart';
-import 'package:my_flutter_app/features/counter/couter_observer.dart';
-import 'package:my_flutter_app/features/timer/view/timer_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// We create a "provider", which will store a value (here "Hello world").
+// By using a provider, this allows us to mock/override the value exposed.
+final helloWorldProvider = Provider((_) => 'Hello world');
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    // For widgets to be able to read providers, we need to wrap the entire
+    // application in a "ProviderScope" widget.
+    // This is where the state of our providers will be stored.
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+// Note: MyApp is a HookConsumerWidget, from hooks_riverpod.
+class MyApp extends HookConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Timer Widget')),
+        appBar: AppBar(title: const Text('Example')),
         body: Center(
-          child: CounterApp(),
+          child: Text(value),
         ),
       ),
     );
