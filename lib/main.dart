5e871796161343f06b901d65e65bd133e110e7f3
hoangdv198 <hoangdv198@gmail.com>
2023-07-03 11:30:04 +0700

    update base

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-// We create a "provider", which will store a value (here "Hello world").
-// By using a provider, this allows us to mock/override the value exposed.
-final helloWorldProvider = Provider((_) => 'Hello world');
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(
-    // For widgets to be able to read providers, we need to wrap the entire
-    // application in a "ProviderScope" widget.
-    // This is where the state of our providers will be stored.
-    ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(ProviderScope(child: MyApp()));
 }
 
-// Note: MyApp is a HookConsumerWidget, from hooks_riverpod.
-class MyApp extends HookConsumerWidget {
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final String value = ref.watch(helloWorldProvider);
+final todoListProvider = StateNotifierProvider<TodoList, List<String>>(
+  (ref) => TodoList(),
+);
 
+class TodoList extends StateNotifier<List<String>> {
+  TodoList() : super([]);
+
+  void add(String todo) {
+    state = [...state, todo];
+  }
+
+  void remove(String todo) {
+    state = state.where((t) => t != todo).toList();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Example')),
-        body: Center(
-          child: Text(value),
-        ),
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TodoListView(),
+    );
+  }
+}
+
+class TodoListView extends ConsumerStatefulWidget {
+  const TodoListView({Key? key}) : super(key: key);
+
+  @override
+  TodoListState createState() => TodoListState();
+}
+
+class TodoListState extends ConsumerState<TodoListView> {
+  @override
+  void initState() {
+    super.initState();
+    // "ref" can be used in all life-cycles of a StatefulWidget.
+  }
+
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final todos = ref.watch(todoListProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                labelText: 'New Todo',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(todoListProvider.notifier).add(_controller.text);
+              _controller.clear();
+            },
+            child: Text('Add Todo'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                final todo = todos[index];
+
+                return ListTile(
+                  title: Text(todo),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      ref.read(todoListProvider.notifier).remove(todo);
+                    },
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
