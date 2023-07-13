7d653f88fdf10e9622b463806721e010336f196a
hoangdv198 <hoangdv198@gmail.com>
2023-07-14 01:42:57 +0700

    update header

--- lib/features/TabbarScreen1/tabbar_screen1.dart
+++ lib/features/TabbarScreen1/tabbar_screen1.dart
@@ -1,6 +1,10 @@
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/assets/images/svg/icon_send.dart';
+import 'package:my_flutter_app/assets/images/svg/logo_ig.dart';
+
+import '../../assets/images/svg/icon_camera.dart';
 
 class TabScreen1 extends StatefulWidget {
   const TabScreen1({super.key});
@@ -12,8 +16,41 @@ class TabScreen1 extends StatefulWidget {
 class _TabScreen1State extends State<TabScreen1> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Tab1"),
+    return SafeArea(
+      child: Column(
+        children: [
+          _header()
+        ],
+      ),
+    );
+  }
+}
+
+class _header extends StatelessWidget {
+  const _header({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              iconCamera,
+              SizedBox(
+                child: logoIg,
+                height: 28,
+                ),
+              iconSend
+            ],
+          ),
+        ),
+        Divider(),
+      ],
     );
   }
 }
