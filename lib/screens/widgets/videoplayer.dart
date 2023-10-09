3d42611a896a6a03f6213c6d479e4245ac0254a6
hoangdv198 <hoangdv198@gmail.com>
2023-10-09 15:53:04 +0700

    update

--- lib/screens/widgets/videoplayer.dart
+++ lib/screens/widgets/videoplayer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ext_video_player/ext_video_player.dart';
 import 'package:my_flutter_app/main.dart';
+import 'package:video_player/video_player.dart';
 
 class MyVideoPlayer extends StatefulWidget {
   const MyVideoPlayer({super.key});
@@ -12,11 +12,17 @@ class MyVideoPlayer extends StatefulWidget {
 class _MyVideoPlayerState extends State<MyVideoPlayer> {
   late VideoPlayerController _controller;
   // stream link: 'rtmp://live.restream.io/live/re_7214043_bd59231ee4ca9208ebab'
+  // rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4
+  // http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4
+  // rtsp://zephyr.rtsp.stream/movie?streamKey=2bfa0ad6e2265107ac64ef3d8ff9a01f
+  // rtsp://zephyr.rtsp.stream/pattern?streamKey=c6487a692ff7d9013c5830cd30139df8
+  // http://13.212.200.148:8080/live/f30e5173-4136-4b9d-8478-a09714551999.flv?token=bc7ad819-e77b-48f0-8baa-70379c8f0e52
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
-        'rtmp://live.restream.io/live/re_7214043_bd59231ee4ca9208ebab')
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+        'http://13.212.200.148:8080/live/f30e5173-4136-4b9d-8478-a09714551999.flv?token=bc7ad819-e77b-48f0-8baa-70379c8f0e52'
+        ))
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
@@ -25,9 +31,9 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
       }).onError((error, stackTrace) {
         logger.e(error.toString());
       });
-      _controller.addListener(() {
-        logger.i(_controller.value);
-      });
+    _controller.addListener(() {
+      logger.i(_controller.value);
+    });
   }
 
   @override
@@ -43,7 +49,7 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
       home: Scaffold(
           backgroundColor: Colors.black,
           body: Stack(children: [
-            if (_controller.value.initialized)
+            if (_controller.value.isInitialized)
               Stack(
                 children: [
                   Center(
@@ -64,7 +70,8 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
                       children: [
                         GestureDetector(
                           onTap: () {
-                            _controller.seekTo(_controller.value.position - Duration(seconds: 10));
+                            _controller.seekTo(_controller.value.position -
+                                Duration(seconds: 10));
                           },
                           child: Container(
                             padding: EdgeInsets.all(20),
@@ -75,7 +82,8 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
                         ),
                         GestureDetector(
                           onTap: () {
-                            _controller.seekTo(_controller.value.position + Duration(seconds: 10));
+                            _controller.seekTo(_controller.value.position +
+                                Duration(seconds: 10));
                           },
                           child: Container(
                             padding: EdgeInsets.all(20),
@@ -89,7 +97,7 @@ class _MyVideoPlayerState extends State<MyVideoPlayer> {
                   ),
                 ],
               ),
-            if (!_controller.value.initialized)
+            if (!_controller.value.isInitialized)
               const Center(child: CircularProgressIndicator()),
           ])),
     );
